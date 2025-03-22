514e2a88a83703afbb10b39eb74e916a88f54777
kentjanuar <c14220124@john.petra.ac.id>
2025-03-22 19:28:22 +0700

    sw ganti

--- sw.js
+++ sw.js
@@ -1,50 +1,57 @@
-self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
-  self.skipWaiting(); //PENTING bila ada versi baru!!
+const CACHE_STATIC_NAME = 'static-v1';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+
+self.addEventListener('install', event => {
+  console.log('[Service Worker] Installing Service Worker ...');
+  // Precache essential assets
   event.waitUntil(
-    caches.open('static')
-      .then(function(cache) {
-        console.log('[Service Worker] Precaching App Shell');
-        cache.addAll([
-          '/',
-          '/index.html',
-          '/src/js/app.js',
-          '/src/js/feed.js',
-          '/src/js/promise.js',
-          '/src/js/fetch.js',
-          '/src/js/material.min.js',
-          '/src/css/app.css',
-          '/src/css/feed.css',
-          '/src/images/main-image.jpg',
-          'https://fonts.googleapis.com/css?family=Roboto:400,700',
-          'https://fonts.googleapis.com/icon?family=Material+Icons',
-          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-        ]);
-      })
-  )
+    caches.open(CACHE_STATIC_NAME).then(cache => {
+      console.log('[Service Worker] Precaching App Shell');
+      return cache.addAll([
+        '/index.html',
+        '/src/js/app.js',
+        '/src/css/app.css',
+        '/offline.html',
+        // etc...
+      ]);
+    })
+  );
+  self.skipWaiting(); // if you want the new SW to activate immediately
 });
 
-
-self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ....', event);
+self.addEventListener('activate', event => {
+  console.log('[Service Worker] Activating Service Worker ...');
+  event.waitUntil(
+    // Optionally remove old caches
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  // Ensure this SW is the controlling one
   return self.clients.claim();
 });
 
-
-
-
-
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.open(CACHE_DYNAMIC_NAME)
-        .then(function(cache) {
-          return fetch(event.request)
-            .then(function(res) {
-              cache.put(event.request, res.clone());
-              return res;
-            });
-        })
-    );
-  });
-  
-  
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        // If we have it in cache, return it
+        return cachedResponse;
+      }
+      // Otherwise, fetch from network and dynamically cache
+      return fetch(event.request).then(networkResponse => {
+        return caches.open(CACHE_DYNAMIC_NAME).then(cache => {
+          cache.put(event.request.url, networkResponse.clone());
+          return networkResponse;
+        });
+      }).catch(err => {
+        // If network fetch fails, optionally serve offline fallback
+        return caches.match('/offline.html');
+      });
+    })
+  );
+});
